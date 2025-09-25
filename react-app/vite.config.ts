b6e1696533ea89b322900c8ea4f159fc2180bc59
48111030 <48111030@est.ort.edu.ar>
2025-09-24 22:23:46 -0300

    detalles para que se hostee bien

--- react-app/vite.config.ts
+++ react-app/vite.config.ts
@@ -1,11 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
-  root: 'react-app',  // ← Add this line!
-  build: {
-    outDir: '../dist',  // ← And this line!
-  },
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true
+  }
 })
